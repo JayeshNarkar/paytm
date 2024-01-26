f87533ee314436bccaccb39af0146bda980a91c7
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-01-26 19:15:52 +0530

    did some checks and corrected the routes as necessary

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
-const mongo_url = process.env.mongo_url
-mongoose.connect(mongo_url+"paytm");
+const mongo_url = process.env.MONGO_URL;
+mongoose.connect(mongo_url + "paytm");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const User = new Schema({
-    id: ObjectId,
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String
+  id: ObjectId,
+  username: {
+    unique: true,
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    minlength: 6,
+    maxlength: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+    maxlength: 20,
+  },
+  firstName: { type: String, required: true, trim: true, maxLength: 50 },
+  lastName: { type: String, required: true, trim: true, maxLength: 50 },
 });
+
 const UserModel = mongoose.model("User", User);
-module.exports = {UserModel};
+
+module.exports = { UserModel };
