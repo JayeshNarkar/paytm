aaa47c7cf2d7363ee26367ee7339588391d196cd
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-01-27 22:04:12 +0530

    added vercel.json and some other features

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
-app.use("/api/v1", rootRouter);
+app.use("/v1", rootRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
