b5fd400cc4138d02c5bb3582008ca264aae55a87
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-01-27 22:21:52 +0530

    fixed some stuff

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const rootRouter = require("./routes/index");
 const app = express();
 const cors = require("cors");
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use("/v1", rootRouter);
@@ -11,10 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-export default app;
