00ab9d52dc59ed62a44c26936c96826132db7d15
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-02-02 00:30:55 +0530

    removed cors from backend

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
 const app = express();
-const cors = require("cors");
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use("/api/v1", rootRouter);
 
 app.use((err, req, res, next) => {
