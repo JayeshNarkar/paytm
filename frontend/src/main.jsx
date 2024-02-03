a1a72bd21ff03b20195342913a2c06ad3f61a383
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-02-03 20:57:03 +0530

    added loader

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useState, useEffect } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
-);
+function Main() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer); // cleanup on unmount
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <PulseLoader color="#123abc" loading={true} size={10} />
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
